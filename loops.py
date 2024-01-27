b1792e765b4fabe51b784ff44b423df24004104d
Jarbas Pereira de Souza <jpsdelarue@gmail.com>
2024-01-27 09:30:35 -0300

    Initial commit

---
+++ loops.py
@@ -0,0 +1,12 @@
+
+print('\n' +'-'*10 + ' Estrutura de Repetição ' +'-'*10 +'\n')
+
+# Itera de 1 ate 5 e ignora o ultimo valor
+for c in range(1,6):
+    print(c)
+print('\n Fim \n')
+
+# Contagem Regressiva
+for c in range(6, 0, -1):
+    print(c)
+print('\n Final')
